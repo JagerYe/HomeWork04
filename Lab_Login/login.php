73a87b06144b3cf9b6f7fc9ca12c30bf283478c8
JagerYe <jager_ye@mail.chungyo.net>
2020-08-20 23:16:40 +0800

    Added account and password blank prompt function

--- Lab_Login/login.php
+++ Lab_Login/login.php
@@ -1,23 +1,41 @@
 <?php
 session_start();
-//登出動作
-if (isset($_SESSION["txtUserName"])) {
-  unset($_SESSION["txtUserName"]);
-  unset($_SESSION["txtPassword"]);
+//登入驗證
+if (isset($_POST["btnOK"])) {
+  clearSessionDate();
+  $userName = $_POST["txtUserName"];
+  $userPassword = $_POST["txtPassword"];
+  $_SESSION["txtUserName"] = $userName;
+  $_SESSION["txtPassword"] = $userPassword;
+  if ($userName == "" || $userPassword == "") {
+    $_SESSION["loginPrompt"] = true;
+    header("Location: login.php");
+    exit();
+  }
+
   header("Location: {$_SESSION['from']}");
   exit();
 }
-//登入驗證
-if (isset($_POST["btnOK"])) {
-  $_SESSION["txtUserName"] = $_POST["txtUserName"];
-  $_SESSION["txtPassword"] = $_POST["txtPassword"];
+if (isset($_SESSION["loginPrompt"])) {
+  echo ("<script>alert('帳號密碼請勿留白')</script>");
+}
+//登出動作
+if (isset($_SESSION["txtUserName"]) && !isset($_SESSION["loginPrompt"])) {
+  clearSessionDate();
   header("Location: {$_SESSION['from']}");
   exit();
 }
 //回首頁
-if(isset($_POST["btnHome"])){
+if (isset($_POST["btnHome"])) {
+  clearSessionDate();
   header("Location: index.php");
   exit();
+}
+function clearSessionDate()
+{
+  unset($_SESSION["loginPrompt"]);
+  unset($_SESSION["txtUserName"]);
+  unset($_SESSION["txtPassword"]);
 }
 ?>
 <html>
@@ -37,7 +55,7 @@ if(isset($_POST["btnHome"])){
       </tr>
       <tr>
         <td width="80" align="center" valign="baseline">帳號</td>
-        <td valign="baseline"><input type="text" name="txtUserName" id="txtUserName" /></td>
+        <td valign="baseline"><input type="text" name="txtUserName" id="txtUserName" value="<?= (isset($_SESSION["txtUserName"]) ? $_SESSION["txtUserName"] : "") ?>" /></td>
       </tr>
       <tr>
         <td width="80" align="center" valign="baseline">密碼</td>
